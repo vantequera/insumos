5eaf514aa60d5f4021f7142fd7388355aa68be48
Ruben Hernandez <ruben_hernandez1993@hotmail.es>
2022-03-23 15:37:09 -0500

    Creacion de modelos para el proyecto Insumos

--- insumos/settings.py
+++ insumos/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'insumos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'insumos',
+        'USER': 'lesavant',
+        'PASSWORD': '2209',
+#        'HOST': 'localhost',
+        'HOST': '192.168.88.100',
+        'PORT': '5432',
     }
 }
 
